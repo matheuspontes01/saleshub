824f5534a3b643b0a85a24cadae6d76f356888d0
matheuspontes01 <matheus.pontes@mail.uft.edu.br>
2026-01-29 09:30:19 -0300

    Projeto Spring Boot criado

---
+++ src/test/java/com/estudo/saleshub/SaleshubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.saleshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleshubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
