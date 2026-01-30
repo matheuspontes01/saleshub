472d76b9b065445a7a454d010e00918b08a4f935
matheuspontes01 <matheus.pontes@mail.uft.edu.br>
2026-01-30 15:32:09 -0300

    findById - Tratamento de exceção feito

--- src/main/java/com/estudo/saleshub/services/UserService.java
+++ src/main/java/com/estudo/saleshub/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.estudo.saleshub.entities.User;
 import com.estudo.saleshub.repositories.UserRepository;
+import com.estudo.saleshub.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
